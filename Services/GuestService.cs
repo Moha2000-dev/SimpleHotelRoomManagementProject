413d3f568d5ec9afb4fcd61a2c5749ca43df3db9
MOHAMMED <mohaalialalawi20@gmail.com>
2025-08-07 09:59:37 +0400

    add the gusets sevres

--- Services/GuestService.cs
+++ Services/GuestService.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using SimpleHotelRoomManagementProject.Models;
+using SimpleHotelRoomManagementProject.Repositories;
 
 namespace SimpleHotelRoomManagementProject.Services
 {
-    class GuestService
+    public class GuestService : IGuestService
     {
+        private readonly IGuestRepository guestRepository;
+
+        public GuestService(IGuestRepository guestRepository)
+        {
+            this.guestRepository = guestRepository;
+        }
+
+        public List<Guest> GetAllGuests()
+        {
+            return guestRepository.GetAllGuests();
+        }
+
+        public Guest GetGuestById(int guestId)
+        {
+            return guestRepository.GetGuestById(guestId);
+        }
+
+        public void AddGuest(Guest guest)
+        {
+            guestRepository.AddGuest(guest);
+        }
+
+        public void UpdateGuest(Guest guest)
+        {
+            guestRepository.UpdateGuest(guest);
+        }
+
+        public void DeleteGuest(int guestId)
+        {
+            guestRepository.DeleteGuest(guestId);
+        }
     }
 }
--- Services/IGuestService.cs
+++ Services/IGuestService.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using SimpleHotelRoomManagementProject.Models;
 
 namespace SimpleHotelRoomManagementProject.Services
 {
-    interface IGuestService
+    public interface IGuestService
     {
+        List<Guest> GetAllGuests();
+        Guest GetGuestById(int guestId);
+        void AddGuest(Guest guest);
+        void UpdateGuest(Guest guest);
+        void DeleteGuest(int guestId);
     }
 }
