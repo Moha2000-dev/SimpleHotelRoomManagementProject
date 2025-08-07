9c884cf6d9b48909984599158d5317767b97f4e1
MOHAMMED <mohaalialalawi20@gmail.com>
2025-08-07 10:01:51 +0400

    creat the i room serves

--- Services/RoomService.cs
+++ Services/RoomService.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using SimpleHotelRoomManagementProject.Models;
+using SimpleHotelRoomManagementProject.Repositories;
 
 namespace SimpleHotelRoomManagementProject.Services
 {
-    class RoomService
+    public class RoomService : IRoomService
     {
+        private readonly IRoomRepository roomRepository;
+
+        public RoomService(IRoomRepository roomRepository)
+        {
+            this.roomRepository = roomRepository;
+        }
+
+        public List<Room> GetAllRooms()
+        {
+            return roomRepository.GetAllRooms();
+        }
+
+        public Room GetRoomById(int roomId)
+        {
+            return roomRepository.GetRoomById(roomId);
+        }
+
+        public void AddRoom(Room room)
+        {
+            roomRepository.AddRoom(room);
+        }
+
+        public void UpdateRoom(Room room)
+        {
+            roomRepository.UpdateRoom(room);
+        }
+
+        public void DeleteRoom(int roomId)
+        {
+            roomRepository.DeleteRoom(roomId);
+        }
     }
 }
