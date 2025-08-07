2f716b2941c2f8cfec09d0168b8282676f6c3a68
MOHAMMED <mohaalialalawi20@gmail.com>
2025-08-07 09:24:59 +0400

    add the helpers method

--- Helpers/GuestFileHelper.cs
+++ Helpers/GuestFileHelper.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using HotelManagementSystem.Models;
+using SimpleHotelRoomManagementProject.Models;
 
-namespace SimpleHotelRoomManagementProject.Helpers
+namespace HotelManagementSystem.Helpers
 {
-    class GuestFileHelper
+    public static class GuestFileHelper
     {
+        private static readonly string filePath = "guests.json";
+
+        public static void SaveGuests(List<Guest> guests)
+        {
+            var json = JsonSerializer.Serialize(guests, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+
+        public static List<Guest> LoadGuests()
+        {
+            if (!File.Exists(filePath))
+                return new List<Guest>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Guest>>(json) ?? new List<Guest>();
+        }
     }
 }
