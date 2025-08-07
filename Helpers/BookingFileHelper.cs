4ab3143fac4d32e27d2af9702b9db94f911eb23c
MOHAMMED <mohaalialalawi20@gmail.com>
2025-08-07 09:23:47 +0400

    creat the project structer

---
+++ Helpers/BookingFileHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using HotelManagementSystem.Models;  // Make sure this namespace is correct
+
+namespace HotelManagementSystem.Helpers
+{
+    public static class BookingFileHelper
+    {
+        private static readonly string filePath = "bookings.json";
+
+        public static void SaveBookings(List<Booking> bookings)
+        {
+            var json = JsonSerializer.Serialize(bookings, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+
+        public static List<Booking> LoadBookings()
+        {
+            if (!File.Exists(filePath))
+                return new List<Booking>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Booking>>(json) ?? new List<Booking>();
+        }
+    }
+}
---
+++ Helpers/RoomFileHelper.cs
@@ -0,0 +1,27 @@
+﻿using SimpleHotelRoomManagementProject.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace HotelManagementSystem.Helpers
+{
+    public static class RoomFileHelper
+    {
+        private static readonly string filePath = "rooms.json";
+
+        public static void SaveRooms(List<Room> rooms)
+        {
+            var json = JsonSerializer.Serialize(rooms, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+
+        public static List<Room> LoadRooms()
+        {
+            if (!File.Exists(filePath))
+                return new List<Room>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<Room>>(json) ?? new List<Room>();
+        }
+    }
+}
